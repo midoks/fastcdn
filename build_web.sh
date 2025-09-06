31f4a68a8a215cb0c2da59da81004e0afed3928f
dami <midoks@163.com>
2025-09-06 15:02:20 +0800

    Update build_web.sh

--- build_web.sh
+++ build_web.sh
@@ -6,10 +6,13 @@ echo "web pnpm build start"
 
 echo $curPath
 
-if [ -f $curPath/fastcdn-web/apps/web-naive/dist.zip ] or [ -f $curPath/fastcdn/public/dist.zip ];then
+if [ -f $curPath/fastcdn-web/apps/web-naive/dist.zip ] || [ -f $curPath/fastcdn/public/dist.zip ];
+then
 	web_md5=`md5sum $curPath/fastcdn-web/apps/web-naive/dist.zip | awk '{print $1}'`
 	fastcdn_public_md5=`md5sum $curPath/fastcdn/public/dist.zip | awk '{print $1}'`
 
+	echo "fastcdn:$fastcdn_public_md5"
+	echo "webdev:$web_md5"
 	if [ "$web_md5" == "$fastcdn_public_md5" ];then
 		# rm -rf ${curPath}/fastcdn-web/apps/web-naive/dist.zip
 		echo "web file no change!"
