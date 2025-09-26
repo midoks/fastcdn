16d9722592aa4225afd48ff064b36aa7758da786
dami <midoks@163.com>
2025-09-27 02:15:48 +0800

    up

--- fastcdn/src/web/app/auth/login.rs
+++ fastcdn/src/web/app/auth/login.rs
@@ -1,14 +1,20 @@
-use actix_web::{Responder, get, post, web};
+use actix_web::{Responder, post, web};
 use serde::{Deserialize, Serialize};
-use std::process::Command;
 use std::sync::Arc;
+
+// 必须为所有需要序列化/反序列化的结构体添加derive
+#[derive(Debug, Serialize, Deserialize)] // 添加Debug方便日志记录
+pub struct LoginResponseData {
+    pub token: String,
+}
 
 // 必须为所有需要序列化/反序列化的结构体添加derive
 #[derive(Debug, Serialize, Deserialize)] // 添加Debug方便日志记录
 pub struct LoginResponse {
     pub message: String,
     pub status: i16,
-    pub data: String,
+    pub token: String,
+    // pub data: Option<LoginResponseData>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)] // 接收JSON请求的结构体
@@ -22,7 +28,6 @@ pub async fn login_post(
     req: web::Json<LoginRequest>,
 ) -> Result<impl Responder, Box<dyn std::error::Error>> {
     // println!("{:?}", req);
-
     let mut admin_rpc = fastcdn_common::rpc::client::CommonRpc::admin_rpc().await?;
 
     let rpc_req = fastcdn_common::rpc::fastcdn::AdminLoginRequest {
@@ -36,19 +41,23 @@ pub async fn login_post(
         .await?;
 
     println!("resp:{:?}", resp);
-    if resp.id != 0 {
+    if resp.id == 0 {
         return Ok(web::Json(LoginResponse {
             message: "登陆失败".to_string(),
             status: -1,
-            data: "".to_string(),
+            token: Some(token?),
         }));
     }
 
     let token = fastcdn::utils::jwt::create(&resp.id.to_string());
     // println!("token:{:?}", token);
-    Ok(web::Json(LoginResponse {
+
+    let resp_data = LoginResponse {
         message: "ok".to_string(),
         status: 0,
-        data: token?,
-    }))
+        token: Some(token?),
+    };
+
+    println!("resp_data:{:?}", resp_data);
+    Ok(web::Json(resp_data))
 }
