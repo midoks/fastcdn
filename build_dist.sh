c0c9d66c367032941d54f9889a23064432ba2d27
dami <midoks@163.com>
2026-05-15 00:58:44 +0800

    update

--- build_dist.sh
+++ build_dist.sh
@@ -10,14 +10,21 @@ mkdir -p dist/fastcdn/logs
 mkdir -p dist/fastcdn/configs
 mkdir -p dist/fastcdn/web
 
-mkdir -p dist/fastcdn/fastcdn-api
-mkdir -p dist/fastcdn/fastcdn-api/configs
-mkdir -p dist/fastcdn/fastcdn-api/bin
-mkdir -p dist/fastcdn/fastcdn-api/deploy
-mkdir -p dist/fastcdn/fastcdn-api/logs
-mkdir -p dist/fastcdn/fastcdn-api/data
+cd $curPath/fastcdn && go build -o fastcdn main.go
+cp -rf $curPath/fastcdn/fastcdn $curPath/dist/fastcdn/fastcdn
 
+mkdir -p $curPath/dist/fastcdn/fastcdn-api
+mkdir -p $curPath/dist/fastcdn/fastcdn-api/configs
+mkdir -p $curPath/dist/fastcdn/fastcdn-api/bin
+mkdir -p $curPath/dist/fastcdn/fastcdn-api/deploy
+mkdir -p $curPath/dist/fastcdn/fastcdn-api/logs
+mkdir -p $curPath/dist/fastcdn/fastcdn-api/data
 
 # fastcdn-node
-mkdir -p dist/fastcdn-node/bin
-mkdir -p dist/fastcdn-node/configs
+cd $curPath && mkdir -p dist/fastcdn-node/bin
+cd $curPath mkdir -p dist/fastcdn-node/configs
+
+
+
+cd $curPath/dist/fastcdn && ./fastcdn web
+
